36c8af37047451bd24b509f40cce0a13b9882588
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-08 02:29:29 +0300

    deleted the extra code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import './style.css';
 console.log('Вебпак работает');
-
-// setTimeout(() => {
-//   document.querySelector('.loading').style = 'display: none;';
-//   document.querySelector('.search-failed').style = 'display: flex;';
-// }, 2000);
