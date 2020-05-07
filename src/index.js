02856eeca47b013f003359665822409f43d2fe46
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-07 18:02:34 +0300

    updated homepage code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './style.css';
 console.log('Вебпак работает');
+
+// setTimeout(() => {
+//   document.querySelector('.loading').style = 'display: none;';
+//   document.querySelector('.search-failed').style = 'display: flex;';
+// }, 2000);
