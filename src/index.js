00304e6a007eb62cd31ec395683c1da0c427f4ea
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-10 15:20:20 +0300

    fixed errors

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './style.css';
 console.log('Вебпак работает');
+
+document.querySelector('.header__navigation-button').addEventListener('click', function(event) {
+  //document.querySelector('.header__navigation-container').style = 'display: flex;'
+});
