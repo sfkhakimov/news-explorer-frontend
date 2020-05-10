36463778e4e6bd55e5b07693bfbcbc7afca84334
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-11 00:55:50 +0300

    made mobile design

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import './style.css';
 console.log('Вебпак работает');
 
 document.querySelector('.header__navigation-button').addEventListener('click', function(event) {
-  //document.querySelector('.header__navigation-container').style = 'display: flex;'
+  if(!document.querySelector('.header__navigation-container').classList.contains('header__navigation-container_mobile-active')) {
+    document.querySelector('.header__navigation-container').classList.add('header__navigation-container_mobile-active')
+    document.querySelector('.header').classList.add('header_mobile')
+  } else {
+    document.querySelector('.header__navigation-container').classList.remove('header__navigation-container_mobile-active')
+    document.querySelector('.header').classList.remove('header_mobile')
+  }
 });
