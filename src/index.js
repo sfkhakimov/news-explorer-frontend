dee6aba40e63033733ff6a515172472f28b8f94a
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-11 14:05:39 +0300

    mobile menu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 console.log('Вебпак работает');
 
-document.querySelector('.header__navigation-button').addEventListener('click', function(event) {
+document.querySelector('.header__mobile-input').addEventListener('click', function(event) {
   if(!document.querySelector('.header__navigation-container').classList.contains('header__navigation-container_mobile-active')) {
     document.querySelector('.header__navigation-container').classList.add('header__navigation-container_mobile-active')
     document.querySelector('.header').classList.add('header_mobile')
