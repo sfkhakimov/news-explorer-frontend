30b6d88f021cac07b5f5cc1847a7e76c9402eb57
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-17 23:53:22 +0300

    создал экземпляры попапов

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 import './style.css';
-console.log('Вебпак работает');
 
-document.querySelector('.header__mobile-input').addEventListener('click', function(event) {
-  if(!document.querySelector('.header__navigation-container').classList.contains('header__navigation-container_mobile-active')) {
-    document.querySelector('.header__navigation-container').classList.add('header__navigation-container_mobile-active')
-    document.querySelector('.header').classList.add('header_mobile')
-    document.querySelector('.overlay').classList.add('overlay_active')
-  } else {
-    document.querySelector('.header__navigation-container').classList.remove('header__navigation-container_mobile-active')
-    document.querySelector('.header').classList.remove('header_mobile')
-    document.querySelector('.overlay').classList.remove('overlay_active')
-  }
-});
+import Popup from './js/components/Popup';
+import SignInPopup from './js/components/SignInPopup';
+import SignUpPopup from './js/components/SignUpPopup';
+
+import {
+  POPUP_CLOSE,
+  POPUP_REPLACE,
+  ROOT,
+  POPUP,
+  HEADER_BUTTON,
+  SIGNIN_POPUP,
+  SIGNUP_POPUP,
+  RESULT_POPUP,
+  BUTTON_SIGNUP,
+  BUTTON_SIGNIN,
+  POPUP_IS_OPENED,
+} from './js/constants/popup';
+
+const module = (function () {
+  const popup = new Popup(POPUP_CLOSE, POPUP_REPLACE, ROOT, POPUP, POPUP_IS_OPENED);
+  const signInPopup = new SignInPopup(HEADER_BUTTON, SIGNIN_POPUP, BUTTON_SIGNIN, popup);
+  const signUpPopup = new SignUpPopup(SIGNUP_POPUP, BUTTON_SIGNUP, popup);
+
+  signInPopup._handler();
+  signUpPopup._handler();
+
+  document.querySelector('.header__mobile-input').addEventListener('click', (event) => {
+    if (!document.querySelector('.header__navigation-container').classList.contains('header__navigation-container_mobile-active')) {
+      document.querySelector('.header__navigation-container').classList.add('header__navigation-container_mobile-active');
+      document.querySelector('.header').classList.add('header_mobile');
+      document.querySelector('.overlay').classList.add('overlay_active');
+    } else {
+      document.querySelector('.header__navigation-container').classList.remove('header__navigation-container_mobile-active');
+      document.querySelector('.header').classList.remove('header_mobile');
+      document.querySelector('.overlay').classList.remove('overlay_active');
+    }
+  });
+}());
