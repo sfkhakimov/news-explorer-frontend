d4dce0385b7ca341d59b269f58b094178d4da391
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-17 23:52:43 +0300

    записал константы для попапа

---
+++ src/js/constants/popup.js
@@ -0,0 +1,25 @@
+const POPUP_CLOSE = 'popup__close';
+const POPUP_REPLACE = 'popup__button-link';
+const ROOT = 'root';
+const POPUP = 'popup';
+const POPUP_IS_OPENED = 'popup_is-opened';
+const HEADER_BUTTON = 'header__button';
+const SIGNIN_POPUP = 'signin';
+const SIGNUP_POPUP = 'signup';
+const RESULT_POPUP = 'signup-result';
+const BUTTON_SIGNUP = 'button-signup';
+const BUTTON_SIGNIN = 'button-signin';
+
+export {
+  POPUP_CLOSE,
+  POPUP_REPLACE,
+  ROOT,
+  POPUP,
+  HEADER_BUTTON,
+  SIGNIN_POPUP,
+  SIGNUP_POPUP,
+  RESULT_POPUP,
+  BUTTON_SIGNUP,
+  BUTTON_SIGNIN,
+  POPUP_IS_OPENED,
+};
