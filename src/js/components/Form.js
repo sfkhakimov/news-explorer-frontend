6505d2961c673f1fa6a12c6d51812b09c71bb9f7
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-20 17:15:00 +0300

    создал класс валидации формы

---
+++ src/js/components/Form.js
@@ -0,0 +1,46 @@
+export default class Form {
+  constructor(obj) {
+    this.errorType = obj.ERROR_TYPE;
+    this.errorLength = obj.ERROR_LENGTH;
+    this.errorServer = obj.ERROR_SERVER;
+    this.errorKeywords = obj.ERROR_KEYWORD;
+    this.errorRequired = obj.ERROR_REQUIRED;
+    this.popupForm = obj.POPUP_FORM;
+    this.popupButton = obj.POPUP_BUTTON;
+    this.buttonActive = obj.BUTTON_ACTIVE;
+    this._setHandlers = this._setHandlers.bind(this);
+    this._validateForm = this._validateForm.bind(this);
+    this._validateInputElement = this._validateInputElement.bind(this);
+  }
+
+  _validateInputElement() {
+    if (event.target.validity.valueMissing) {
+      document.querySelector(`#error-${event.target.id}`).textContent = this.errorRequired;
+    } else if (event.target.validity.tooShort || event.target.validity.tooLong) {
+      document.querySelector(`#error-${event.target.id}`).textContent = this.errorLength;
+    } else if (event.target.validity.typeMismatch) {
+      document.querySelector(`#error-${event.target.id}`).textContent = `${this.errorType} ${event.target.type}`;
+    } else {
+      document.querySelector(`#error-${event.target.id}`).textContent = '';
+    }
+    this._validateForm();
+  }
+
+  _validateForm() {
+    const isValidForm = Array.from(document.querySelector(`.${this.popupForm}`).elements).every((elem) => {
+      return elem.checkValidity();
+    });
+
+    if (isValidForm) {
+      document.querySelector(`.${this.popupButton}`).classList.add(this.buttonActive);
+      document.querySelector(`.${this.popupButton}`).removeAttribute('disabled', true);
+    } else {
+      document.querySelector(`.${this.popupButton}`).classList.remove(this.buttonActive);
+      document.querySelector(`.${this.popupButton}`).setAttribute('disabled', true);
+    }
+  }
+
+  _setHandlers() {
+    document.querySelector(`.${this.popupForm}`).addEventListener('input', this._validateInputElement);
+  }
+}
---
+++ src/js/constants/form.js
@@ -0,0 +1,19 @@
+const ERROR_TYPE = 'Недопустимый формат';
+const ERROR_LENGTH = 'Длина должна быть от 2 до 30 символов';
+const ERROR_SERVER = 'Такой пользователь уже есть';
+const ERROR_KEYWORD = 'Нужно ввести ключевое слово';
+const ERROR_REQUIRED = 'Это обязательное поле';
+const POPUP_FORM = 'popup__form';
+const BUTTON_ACTIVE = 'popup__button_active';
+const POPUP_BUTTON = 'popup__button';
+
+export {
+  ERROR_TYPE,
+  ERROR_LENGTH,
+  ERROR_SERVER,
+  ERROR_KEYWORD,
+  POPUP_FORM,
+  BUTTON_ACTIVE,
+  POPUP_BUTTON,
+  ERROR_REQUIRED,
+};
