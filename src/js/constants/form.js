5f6209942fc4605cb191f39cea44a1a4bb00384c
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-22 13:10:22 +0300

    Поменял константы

--- src/js/constants/form.js
+++ src/js/constants/form.js
@@ -1,11 +1,11 @@
 const ERROR_TYPE = 'Недопустимый формат';
 const ERROR_LENGTH = 'Длина должна быть от 2 до 30 символов';
-const ERROR_SERVER = 'Такой пользователь уже есть';
 const ERROR_KEYWORD = 'Нужно ввести ключевое слово';
 const ERROR_REQUIRED = 'Это обязательное поле';
 const POPUP_FORM = 'popup__form';
 const BUTTON_ACTIVE = 'popup__button_active';
 const POPUP_BUTTON = 'popup__button';
+const ERROR_SERVER = 'popup__error-server';
 
 export {
   ERROR_TYPE,
