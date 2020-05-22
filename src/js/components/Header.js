dba1cad625d075146443e486e71d560d554fe138
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-22 13:05:09 +0300

    Реализовал логику работы шапки

---
+++ src/js/components/Header.js
@@ -0,0 +1,68 @@
+export default class Header {
+  constructor(obj, api) {
+    this.email = null;
+    this.password = null;
+    this.logged = false;
+    this.input = obj.MOBILE_INPUT;
+    this.navigation = obj.NAVIGATION_CONTAINER;
+    this.navigationActive = obj.NAVIGATION_CONTAINER_ACTIVE;
+    this.header = obj.HEADER;
+    this.headerMobile = obj.HEADER_MOBILE;
+    this.overlay = obj.OVERLAY;
+    this.overlayActive = obj.OVERLAY_ACTIVE;
+    this.headerButton = obj.HEADER_BUTTON;
+    this.imageButton = obj.HEADER_BUTTON_IMAGE;
+    this.headerArticle = obj.HEADER_ARTICLE;
+    this.navItemDisplay = obj.NAV_ITEM_DISPLAY_NONE;
+    this.api = api;
+    this.mobileMenu = this.mobileMenu.bind(this);
+    this.render = this.render.bind(this);
+    this.rememberUser = this.rememberUser.bind(this);
+    this.output = this.output.bind(this);
+    this._setHndlers = this._setHndlers.bind(this);
+  }
+
+  render(name) {
+    if (name !== undefined) {
+      this.logged = true;
+      document.querySelector(`.${this.headerButton}`).textContent = name;
+      document.querySelector(`.${this.headerButton}`)
+        .append(document.querySelector(`#${this.imageButton}`)
+          .content.cloneNode(true));
+      document.querySelector(`#${this.headerArticle}`).classList.remove(this.navItemDisplay);
+    } else {
+      this.logged = false;
+      document.querySelector(`.${this.headerButton}`).textContent = 'Авторизоваться';
+      document.querySelector(`#${this.headerArticle}`).classList.add(this.navItemDisplay);
+    }
+  }
+
+  rememberUser(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+
+  mobileMenu() {
+    if (!document.querySelector(`.${this.navigation}`).classList.contains(this.navigationActive)) {
+      document.querySelector(`.${this.navigation}`).classList.add(this.navigationActive);
+      document.querySelector(`.${this.header}`).classList.add(this.headerMobile);
+      document.querySelector(`.${this.overlay}`).classList.add(this.overlayActive);
+    } else {
+      document.querySelector(`.${this.navigation}`).classList.remove(this.navigationActive);
+      document.querySelector(`.${this.header}`).classList.remove(this.headerMobile);
+      document.querySelector(`.${this.overlay}`).classList.remove(this.overlayActive);
+    }
+  }
+
+  output() {
+    this.api.logout()
+      .then((res) => {
+        this.render();
+      })
+      .catch((err) => err);
+  }
+
+  _setHndlers() {
+    document.querySelector(`.${this.input}`).addEventListener('click', this.mobileMenu);
+  }
+}
---
+++ src/js/constants/header.js
@@ -0,0 +1,23 @@
+const MOBILE_INPUT = 'header__mobile-input';
+const NAVIGATION_CONTAINER = 'header__navigation-container';
+const NAVIGATION_CONTAINER_ACTIVE = 'header__navigation-container_mobile-active';
+const HEADER = 'header';
+const HEADER_MOBILE = 'header_mobile';
+const OVERLAY = 'overlay';
+const OVERLAY_ACTIVE = 'overlay_active';
+const NAV_ITEM_DISPLAY_NONE = 'nav__item_display_none';
+const HEADER_ARTICLE = 'nav-item-article';
+const HEADER_BUTTON_IMAGE = 'header-button-image';
+
+export {
+  MOBILE_INPUT,
+  NAVIGATION_CONTAINER,
+  NAVIGATION_CONTAINER_ACTIVE,
+  HEADER,
+  HEADER_MOBILE,
+  OVERLAY,
+  OVERLAY_ACTIVE,
+  HEADER_BUTTON_IMAGE,
+  HEADER_ARTICLE,
+  NAV_ITEM_DISPLAY_NONE,
+};
