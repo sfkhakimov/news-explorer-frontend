8d1e46c309521a690df58a9a820ed35bed7607d4
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-22 13:06:03 +0300

    Добавил в попап логику отправки формы

--- src/js/components/Popup.js
+++ src/js/components/Popup.js
@@ -1,6 +1,5 @@
 export default class Popup {
-  constructor(obj, form) {
-    this.form = form;
+  constructor(obj, form, header, mainApi) {
     this.popupClose = obj.POPUP_CLOSE;
     this.replace = obj.POPUP_REPLACE;
     this.root = obj.ROOT;
@@ -10,11 +9,16 @@ export default class Popup {
     this.resultPopup = obj.RESULT_POPUP;
     this.buttonSignUp = obj.BUTTON_SIGNUP;
     this.buttonSignIn = obj.BUTTON_SIGNIN;
+    this.popupButton = obj.POPUP_BUTTON;
+    this.form = form;
+    this.header = header;
+    this.mainApi = mainApi;
     this.open = this.open.bind(this);
     this.setContent = this.setContent.bind(this);
     this.close = this.close.bind(this);
-    this._setHandlers = this._setHandlers.bind(this);
     this.change = this.change.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+    this._setHandlers = this._setHandlers.bind(this);
   }
 
   setContent(elem) {
@@ -26,17 +30,19 @@ export default class Popup {
   }
 
   open() {
-    this.setContent(this.signInPopup);
+    if (this.header.logged === false) {
+      this.setContent(this.signInPopup);
+    } else {
+      this.header.output();
+    }
   }
 
   close() {
     document.querySelector(`.${this.openedPopup}`).remove();
-    if (event.target.classList.contains(this.replace)) {
-      this.change();
-    }
   }
 
   change() {
+    this.close();
     if (event.target.id === this.buttonSignUp) {
       this.setContent(this.signUpPopup);
     } else {
@@ -44,8 +50,34 @@ export default class Popup {
     }
   }
 
+  submitForm() {
+    event.preventDefault();
+    const form = document.querySelector(`.${this.form.popupForm}`);
+    if (form.id === 'authorization') {
+      this.mainApi.signin(form.elements.email.value, form.elements.password.value)
+        .then((user) => {
+          this.header.render(user.name);
+          this.close();
+        })
+        .catch((err) => {
+          this.form.setServerError(err);
+        });
+    } else if (form.id === 'registration') {
+      this.mainApi.signup(form.elements.email.value, form.elements.password.value, form.elements.name.value)
+        .then((res) => {
+          this.header.rememberUser(form.elements.email.value, form.elements.password.value);
+          this.close();
+          this.setContent(this.resultPopup);
+        })
+        .catch((err) => {
+          this.form.setServerError(err);
+        });
+    }
+  }
+
   _setHandlers() {
     document.querySelector(`.${this.popupClose}`).addEventListener('click', this.close);
-    document.querySelector(`.${this.replace}`).addEventListener('click', this.close);
+    document.querySelector(`.${this.replace}`).addEventListener('click', this.change);
+    document.querySelector(`.${this.form.popupForm}`).addEventListener('submit', this.submitForm);
   }
 }
