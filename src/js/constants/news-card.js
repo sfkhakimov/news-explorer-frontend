52c2c14dfb9da918ec9eac79a0b2c0ab975e91bd
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-25 15:40:09 +0300

    создал класс статьи

---
+++ src/js/constants/news-card.js
@@ -0,0 +1,14 @@
+export default {
+  NEWS_CARD: 'news__card',
+  NEWS_CARD_TMPL: 'news-card',
+  NEWS_CARD_DATE: 'news-card__date',
+  LINK: 'link',
+  NEWS_CARD_TITLE: 'news-card__title',
+  NEWS_CARD_DESCRIPTION: 'news-card__description',
+  NEWS_CARD_SOURCE: 'news-card__source',
+  NEWS_CARD_BUTTON: 'news-card__button',
+  NEWS_CARD_IMAGE: 'news-card__image',
+  MOUNTH: ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'],
+  NEWS_CARD_BUTTON_DESCRIPTION: 'news-card__button-description',
+  NEWS_CARD_BUTTON_SAVED: 'news-card__button_saved',
+};
