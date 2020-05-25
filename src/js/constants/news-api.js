4ecbbae123d41a3a9b691889f4f29efbe537b272
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-25 15:42:03 +0300

    переписал константы

--- src/js/constants/news-api.js
+++ src/js/constants/news-api.js
@@ -1,7 +1,7 @@
 const NEWS_URL = 'https://praktikum.tk/news/v2/everything';
 const API_KEY = 'apiKey=1a27ff3603c6422681720b80c8a7cb70';
 const SORT = 'sortBy=popularity';
-const PAGE_SIZE = '100';
+const PAGE_SIZE = 'pageSize=100';
 
 export {
   NEWS_URL,
