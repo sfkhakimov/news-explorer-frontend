0de40916703537b7f0ced1476aaab8800ba53385
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-25 20:35:48 +0300

    добавил метод удаления статьи и отрисовки сохраненных статей

--- src/js/components/NewsCard.js
+++ src/js/components/NewsCard.js
@@ -12,14 +12,18 @@ export default class NewsCard {
     this.mounthArr = obj.MOUNTH;
     this.newsCardButtonDescriotion = obj.NEWS_CARD_BUTTON_DESCRIPTION;
     this.newsCardButtonSaved = obj.NEWS_CARD_BUTTON_SAVED;
+    this.newsCardKeywords = obj.NEWS_CARD_KEYWORDS;
     this.authorization = authorization;
     this.mainApi = mainApi;
     this.articleData = undefined;
     this.keyWord = undefined;
     this.article = undefined;
     this.card = this.card.bind(this);
+    this.deleteArticle = this.deleteArticle.bind(this);
+    this.createSavedCard = this.createSavedCard.bind(this);
     this.saveArticle = this.saveArticle.bind(this);
-    this._setHndlers = this._setHndlers.bind(this);
+    this._setHandlers = this._setHandlers.bind(this);
+    this._savedArticleHandlers = this._savedArticleHandlers.bind(this);
   }
 
   card(elem) {
@@ -43,8 +47,29 @@ export default class NewsCard {
       card.querySelector(`.${this.newsCardButton}`).addEventListener('click', this.saveArticle);
     } else {
       card.querySelector(`.${this.newsCardButton}`).setAttribute('disabled', true);
-      this._setHndlers();
+      this._setHandlers();
     }
+    return card;
+  }
+
+  createSavedCard(elem) {
+    this.articleData = elem;
+    const date = new Date(elem.date).getDate();
+    const mounth = new Date(elem.date).getMonth();
+    const year = new Date(elem.date).getFullYear();
+    const card = document.createElement('div');
+    card.classList.add(this.newsCard);
+    card.append(document.querySelector(`#${this.newsCardTmpl}`).content.cloneNode(true));
+    card.querySelector(`.${this.newsCardDate}`).textContent = `${date} ${this.mounthArr[mounth]}, ${year}`;
+    card.querySelector(`.${this.newsCardTitle}`).textContent = elem.title;
+    card.querySelector(`.${this.newsCardKeywords}`).textContent = elem.keyword;
+    card.querySelector(`.${this.newsCardDescription}`).textContent = elem.text;
+    card.querySelector(`.${this.newsCardSource}`).textContent = elem.source;
+    card.querySelector(`.${this.newsCardImage}`).style.backgroundImage = `url(${elem.image})`;
+    card.querySelector(`.${this.link}`).setAttribute('href', elem.link);
+    card.querySelector(`.${this.newsCardButton}`).removeAttribute('disabled', true);
+    this.article = card;
+    this._savedArticleHandlers();
     return card;
   }
 
@@ -68,7 +93,22 @@ export default class NewsCard {
       .catch((err) => alert(err.message));
   }
 
-  _setHndlers() {
+  deleteArticle() {
+    this.mainApi.removeArticle(this.articleData._id)
+      .then((res) => {
+        if (res.message !== undefined) {
+          Promise.reject(res);
+        }
+        this.article.remove();
+      })
+      .catch((err) => alert(err.message));
+  }
+
+  _savedArticleHandlers() {
+    this.article.querySelector(`.${this.newsCardButton}`).addEventListener('click', this.deleteArticle);
+  }
+
+  _setHandlers() {
     this.article.querySelector(`.${this.newsCardButton}`).addEventListener('click', this.saveArticle);
   }
 }
