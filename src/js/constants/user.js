b83a55bbc06202f99e5835893f189525a01b0ba8
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-05-25 20:43:03 +0300

    коздал константы для класса

---
+++ src/js/constants/user.js
@@ -0,0 +1,9 @@
+export default {
+  ARTILES_TITLE: 'saved-articles__subtitle',
+  ARTICLES_DESCRIPTION: 'saved-articles__description',
+  ARTICLES_KEYWORDS: 'saved-articles__keywrods',
+  ARTICLES_DESCRIPTION_ACTIVE: 'saved-articles__description_active',
+  ARTICLE_ONE: 'article-one',
+  ARTICLE_TWO: 'article-two',
+  ARTICLE_OTHER: 'article-other',
+};
